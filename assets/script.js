93cc821e530fc124cf554c40a2854689450f9453
trista_lea <tristan_lea@outlook.com>
2022-10-28 11:32:22 -0700

    fleshed out a lot of it

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,44 @@
-const question1 = document.querySelector('.question');
+//question current index
+//timeLength = function(array(num of questions))
+//timer id? 
+//time variable to be printed
+//.textContent()
+
+const question1 = document.querySelector('.questionPrompt');
 const question2 = document.querySelector('.question');
 const question3 = document.querySelector('.question');
+
+queAra[
+    {
+        questionss:"", 
+        choicesAra:["",""],
+        answers:[""],
+    },
+    {
+        questionss:"", 
+        choicesAra:["",""],
+        answers:[""],
+    }]
+
+//contains all answers
 const choices = Array(document.querySelectorAll('.choices'));
+
+//highscores
+//grabbing initials
+//submit
+//starting
+
+//fun start quiz starts timer
+//timer
+//set attributes for displaying and updating
+
+//deselceter funtion
+
+//the last bit is just easy math functions to calc socre,
+//how do I save the highscore?
+
+//fucntions grab current questions
+// { set attribute to current container of questions}
+// queAra is numerable
+
+//local storage for highscore saving
