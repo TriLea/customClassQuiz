1666f406dc872c66bf3147adc4f116a40a82b305
trista_lea <tristan_lea@outlook.com>
2022-10-27 19:13:06 -0700

    another quick save, got stuck going to go study

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const question1 = document.querySelector('.question');
+const question2 = document.querySelector('.question');
+const question3 = document.querySelector('.question');
+const choices = Array(document.querySelectorAll('.choices'));
